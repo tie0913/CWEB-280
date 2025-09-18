cdaa281f059836311eda7d1f5dfd03a6c61fd5f6
Tie Wang <wang9431@saskpolytech.ca>
2025-09-18 10:59:22 -0600

    add copy to clipboard

--- assignment01/public/javascripts/invites.js
+++ assignment01/public/javascripts/invites.js
@@ -1,5 +1,7 @@
 
 function createInvites(eventId){
+
+    document.getElementById("copyButton").innerText = "Copy";
     fetch('/events/invites?eventId='+eventId)
     .then(res => res.json())
     .then(data => {
@@ -7,4 +9,18 @@ function createInvites(eventId){
         document.getElementById("dialog").style.display = "block"
     })
     .catch(err => console.error(err));
+}
+
+
+function closeInvitesDialog(){
+    document.getElementById("dialog").style.display = "none"
+}
+
+function copyToClipboard(){
+    let text = document.getElementById("token_p").innerText;
+    navigator.clipboard.writeText(text);
+    document.getElementById("copyButton").innerText = "Copied";
+    window.setTimeout(function(){
+        closeInvitesDialog()
+    }, 200);
 }
